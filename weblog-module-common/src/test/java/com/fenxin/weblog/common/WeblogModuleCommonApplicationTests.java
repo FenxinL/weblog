bbdf95d7ab7c6e3e3654b9f512f54675f31c0cb8
13284 <1328414279@qq.com>
2024-06-05 21:42:03 +0800

    3.1 3.2over√

---
+++ weblog-module-common/src/test/java/com/fenxin/weblog/common/WeblogModuleCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fenxin.weblog.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblogModuleCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
